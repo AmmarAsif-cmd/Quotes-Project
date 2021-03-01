2ecdcb7d3a54d8f465f8cf380950da3722266796
Zia.Din-External <ammarasif550@gmail.com>
2021-03-01 17:14:53 +0500

    your-msg

---
+++ pages/quotes/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="w-3/4 m-auto">
+    <h1 class="bg-green-700 text-white p-3">Ammar Asif</h1>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style>
+/* Aven e bs */
+</style>
